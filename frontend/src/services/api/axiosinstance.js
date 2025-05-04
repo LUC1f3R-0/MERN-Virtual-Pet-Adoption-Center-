d74cfae8379117b43f1eb5349166a806c16ca988
Thushara_thilan <142692846+LUC1f3R-0@users.noreply.github.com>
2025-05-04 18:26:18 +0530

    Delete frontend directory

--- frontend/src/services/api/axiosinstance.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-const axios_Instance = new axios.create({
-    baseURL: import.meta.env.VITE_BACKEND_ENTRYPOINT,
-    timeout: 5000,
-    headers: { 'Content-Type': 'application/json' }
-})
-
-export default axios_Instance
